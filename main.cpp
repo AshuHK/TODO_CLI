0acc706118071f7ded70f2136c7d2406780e7e9a
Ashu Kher <ashukher1@gmail.com>
2020-06-16 10:40:13 -0400

    basically finished the input system

--- main.cpp
+++ main.cpp
@@ -20,39 +20,38 @@ int main() {
   std::cout << "Welcome to TODO CLI" << std::endl;
   std::cout << "Your command line to-do list :)" << std::endl;
 
-  // std::string input;
-  // while (std::getline(std::cin, input)) {
-  //   if (input.empty()) {
-  //     std::cout << "type something boi" << std::endl;
-  //   }
-  // }
-
   while (true) {
     std::string input;
     std::getline(std::cin, input);
 
     if (!input.empty()) {
       switch (input[0]) {
+        case 'L':
         case 'l':
           std::cout << "l - list items in list" << std::endl;
           break;
 
+        case 'A':
         case 'a':
           std::cout << "a - add new item to the list" << std::endl;
           break;
 
+        case 'C':
         case 'c':
           std::cout << "c - complete/check off item in the list" << std::endl;
           break;
 
+        case 'D':
         case 'd':
           std::cout << "d - delete item in the list" << std::endl;
           break;
 
+        case 'H':
         case 'h':
           std::cout << "h - help (print the keybindings)" << std::endl;
           break;
 
+        case 'Q':
         case 'q':
           std::cout << "q - quit TODO CLI" << std::endl;
           return 0;

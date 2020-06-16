09dc79931d317839d496bfac7358b7a1f5a013e6
Ashu Kher <ashukher1@gmail.com>
2020-06-16 09:49:02 -0400

    made makefile

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <fstream>
 #include <iostream>
 
+/**
+ * Keybindings 
+ * 
+ * main controls 
+ * l - list items in list 
+ * a - add new item to the list 
+ * c - complete/check off item in the list 
+ * d - delete item in the list
+ * 
+ * misc. keybindings 
+ * h - help (print the keybindings)
+ * q - quit 
+ */
+
 int main() {
-  std::cout << "What it do babyyy?" << std::endl;
+  std::cout << "Welcome to TODO CLI" << std::endl;
+  std::cout << "Your command line to-do list :)" << std::endl;
+
   return 0;
 }

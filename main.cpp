f77664f47181a5b75848f577e74de54ab999ecca
Ashu Kher <ashukher1@gmail.com>
2020-06-16 11:20:06 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,31 +2,16 @@
 #include <iostream>
 #include <string>
 
-/**
- * Keybindings
- *
- * main controls
- * l - list items in list
- * a - add new item to the list
- * c - complete/check off item in the list
- * d - delete item in the list
- *
- * misc. keybindings
- * h - help (print the keybindings)
- * q - quit TODO CLI
- */
-
 int main() {
   std::cout << "Welcome to TODO CLI" << std::endl;
   std::cout << "Your command line to-do list :)" << std::endl;
 
   while (true) {
-    std::cout << ">> "; 
+    std::cout << ">> ";
     std::string input;
     std::getline(std::cin, input);
 
     if (!input.empty()) {
-
       // switch is based on the first char in the input
       switch (input[0]) {
         case 'L':
